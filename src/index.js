6373a52259ae1b63f3a4ffb2009c6114350a50ec
Cory Bass <cory.bass.07@gmail.com>
2021-04-15 19:59:58 -0500

    added in track routes and model for route coords

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require("./models/User");
+require("./models/Track");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const { PASSWORD } = require("../mongoDBCredentials/credentials");
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const credentials = require("../mongoDBCredentials/credentials");
 
