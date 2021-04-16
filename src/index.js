7b66499021744a30bc0bd6a9f3f98806f6c96288
Cory Bass <cory.bass.07@gmail.com>
2021-04-15 19:02:40 -0500

    db returning values, TODO: encrypt password

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
 const { PASSWORD } = require("../mongoDBCredentials/credentials");
 
 const app = express();
@@ -23,6 +25,14 @@ mongoose.connection.on("connected", () => {
 });
 mongoose.connection.on("error", (err) => {
     console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+    try {
+        res.send(`Your email is: ${req.user.email}`);
+    } catch (err) {
+        return res.status(401).send(err.message);
+    }
 });
 
 app.listen(3000, () => {
